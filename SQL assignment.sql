1ecca9344aa3febb210209bb004ab9f7fe3fa616
erictuscanes <41929437+erictuscanes@users.noreply.github.com>
2018-10-18 16:19:34 -0400

    Latest commit.

--- SQL assignment.sql
+++ SQL assignment.sql
@@ -5,7 +5,7 @@ SELECT first_name, last_name
 FROM actor;
 
 #1b. Display the first and last name of each actor in a single column in upper case letters. Name the column Actor Name.
-SELECT UPPER(CONCAT(first_name, ' ', last_name)) AS actor_name
+SELECT UPPER(CONCAT(first_name, ' ', last_name)) AS "Actor Name"
 FROM actor;
 
 #2a. You need to find the ID number, first name, and last name of an actor, of whom you know only the first name, "Joe." What is one query would you use to obtain this information?
@@ -18,10 +18,9 @@ SELECT first_name, last_name from actor
 WHERE last_name ="%GEN%";
 
 #2c. Find all actors whose last names contain the letters LI. This time, order the rows by last name and first name, in that order:
-SELECT actor_id, first_name, last_name 
+SELECT actor_id, last_name, first_name
 FROM actor
 WHERE last_name LIKE '%LI%'
-ORDER BY last_name, first_name;
 
 #2d. Using IN, display the country_id and country columns of the following countries: Afghanistan, Bangladesh, and China:
 SELECT country_id,  country
@@ -29,7 +28,7 @@ FROM country
 WHERE country IN ('Afghanistan', 'Bangladesh', 'China');
 
 #4a. List the last names of actors, as well as how many actors have that last name.
-SELECT last_name, COUNT(*) AS `Count`
+SELECT last_name, COUNT(*) AS 'Count'
 FROM actor
 GROUP BY last_name;
 
@@ -40,19 +39,28 @@ FROM actor GROUP BY last_name HAVING count(*) >=2;
 #4c. The actor HARPO WILLIAMS was accidentally entered in the actor table as GROUCHO WILLIAMS. Write a query to fix the record.
 UPDATE actor 
 SET first_name = 'HARPO'
-WHERE First_name = "Groucho" AND last_name = "Williams";
+WHERE First_name = 'Groucho' AND last_name = 'Williams';
 
+#4d. Perhaps we were too hasty in changing GROUCHO to HARPO. It turns out that GROUCHO was the correct name after all! In a single query, if the first name of the actor is currently HARPO, change it to GROUCHO.
+UPDATE actor 
+SET first_name= 'GROUCHO'
+WHERE first_name='HARPO' AND last_name='WILLIAMS';
+
+#5a. You cannot locate the schema of the address table. Which query would you use to re-create it?
 DESCRIBE sakila.address;
 
+#6a. Use JOIN to display the first and last names, as well as the address, of each staff member. Use the tables staff and address:
 SELECT s.first_name, s.last_name, a.address
-FROM staff s LEFT JOIN address a ON s.address_id = a.address_id;
+FROM staff s JOIN address a ON s.address_id = a.address_id;
 
+#6b. Use JOIN to display the total amount rung up by each staff member in August of 2005. Use tables staff and payment.
 SELECT s.first_name, s.last_name, SUM(p.amount) AS 'TOTAL'
 FROM staff s LEFT JOIN payment p  ON s.staff_id = p.staff_id
 GROUP BY s.first_name, s.last_name;
 
+#6c. List each film and the number of actors who are listed for that film. Use tables film_actor and film. Use inner join.
 SELECT f.title, COUNT(a.actor_id) AS 'TOTAL'
-FROM film f LEFT JOIN film_actor  a ON f.film_id = a.film_id
+FROM film f INNER JOIN film_actor  a ON f.film_id = a.film_id
 GROUP BY f.title;
 
 SELECT c.first_name, c.last_name, SUM(p.amount) AS 'TOTAL'
@@ -60,30 +68,31 @@ FROM customer c LEFT JOIN payment p ON c.customer_id = p.customer_id
 GROUP BY c.first_name, c.last_name
 ORDER BY c.last_name;
 
-#7A
+#7a. The music of Queen and Kris Kristofferson have seen an unlikely resurgence. As an unintended consequence, films starting with the letters K and Q have also soared in popularity. Use subqueries to display the titles of movies starting with the letters K and Q whose language is English.
 SELECT title
 FROM film
 WHERE (title LIKE 'K%' OR title LIKE 'Q%') 
 AND language_id=(SELECT language_id FROM language where name='English');
 
-#7B
+#7b. Use subqueries to display all actors who appear in the film Alone Trip.
 SELECT first_name, last_name
 FROM actor
 WHERE actor_id
 	IN (SELECT actor_id FROM film_actor WHERE film_id 
 		IN (SELECT film_id from film where title='ALONE TRIP'));
         
-#7C
+#7c. You want to run an email marketing campaign in Canada, for which you will need the names and email addresses of all Canadian customers. Use joins to retrieve this information.
 SELECT first_name, last_name, email 
 FROM customer cu
-JOIN address a ON (cu.address_id = a.address_id)
+JOIN address a ON (cu.address_id = a.address_id)country
 JOIN city cit ON (a.city_id=cit.city_id)
 JOIN country cntry ON (cit.country_id=cntry.country_id);
 
-#7D
-SELECT title from film f
-JOIN film_category fcat on (f.film_id=fcat.film_id)
-JOIN category c on (fcat.category_id=c.category_id);
+#7d. Sales have been lagging among young families, and you wish to target all family movies for a promotion. Identify all movies categorized as family films.
+SELECT title 
+FROM film 
+WHERE film_id 
+IN (SELECT film_id FROM film_category WHERE category_id IN (SELECT category_id FROM category WHERE name = 'Family'));
 
 SELECT title, COUNT(f.film_id) AS 'Count_of_Rented_Movies'
 FROM  film f
