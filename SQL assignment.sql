f66cb19b9664a328e7ed4bfe93cea0d9fafa58bd
erictuscanes <41929437+erictuscanes@users.noreply.github.com>
2018-10-19 12:51:58 -0400

    Completed assignment.

--- SQL assignment.sql
+++ SQL assignment.sql
@@ -20,12 +20,22 @@ WHERE last_name ="%GEN%";
 #2c. Find all actors whose last names contain the letters LI. This time, order the rows by last name and first name, in that order:
 SELECT actor_id, last_name, first_name
 FROM actor
-WHERE last_name LIKE '%LI%'
+WHERE last_name LIKE '%LI%';
 
 #2d. Using IN, display the country_id and country columns of the following countries: Afghanistan, Bangladesh, and China:
 SELECT country_id,  country
 FROM country
 WHERE country IN ('Afghanistan', 'Bangladesh', 'China');
+
+#3a. You want to keep a description of each actor. 
+#You don't think you will be performing queries on a description, 
+#so create a column in the table actor named description and use the data type BLOB.
+ALTER TABLE actor
+ADD COLUMN description BLOB(65535);
+
+#3b. Very quickly you realize that entering descriptions for each actor is too much effort. Delete the description column.
+ALTER TABLE actor
+DROP COLUMN description;
 
 #4a. List the last names of actors, as well as how many actors have that last name.
 SELECT last_name, COUNT(*) AS 'Count'
@@ -41,7 +51,8 @@ UPDATE actor
 SET first_name = 'HARPO'
 WHERE First_name = 'Groucho' AND last_name = 'Williams';
 
-#4d. Perhaps we were too hasty in changing GROUCHO to HARPO. It turns out that GROUCHO was the correct name after all! In a single query, if the first name of the actor is currently HARPO, change it to GROUCHO.
+#4d. Perhaps we were too hasty in changing GROUCHO to HARPO. 
+#It turns out that GROUCHO was the correct name after all! In a single query, if the first name of the actor is currently HARPO, change it to GROUCHO.
 UPDATE actor 
 SET first_name= 'GROUCHO'
 WHERE first_name='HARPO' AND last_name='WILLIAMS';
@@ -78,13 +89,13 @@ AND language_id=(SELECT language_id FROM language where name='English');
 SELECT first_name, last_name
 FROM actor
 WHERE actor_id
-	IN (SELECT actor_id FROM film_actor WHERE film_id 
-		IN (SELECT film_id from film where title='ALONE TRIP'));
+IN (SELECT actor_id FROM film_actor WHERE film_id 
+IN (SELECT film_id from film where title='ALONE TRIP'));
         
 #7c. You want to run an email marketing campaign in Canada, for which you will need the names and email addresses of all Canadian customers. Use joins to retrieve this information.
 SELECT first_name, last_name, email 
 FROM customer cu
-JOIN address a ON (cu.address_id = a.address_id)country
+JOIN address a ON (cu.address_id = a.address_id)
 JOIN city cit ON (a.city_id=cit.city_id)
 JOIN country cntry ON (cit.country_id=cntry.country_id);
 
@@ -94,23 +105,27 @@ FROM film
 WHERE film_id 
 IN (SELECT film_id FROM film_category WHERE category_id IN (SELECT category_id FROM category WHERE name = 'Family'));
 
+#7e. Display the most frequently rented movies in descending order.
 SELECT title, COUNT(f.film_id) AS 'Count_of_Rented_Movies'
 FROM  film f
 JOIN inventory i ON (f.film_id= i.film_id)
 JOIN rental r ON (i.inventory_id=r.inventory_id)
 GROUP BY title ORDER BY Count_of_Rented_Movies DESC;
 
+#7f. Write a query to display how much business, in dollars, each store brought in.
 SELECT s.store_id, SUM(p.amount) 
 FROM payment p
 JOIN staff s ON (p.staff_id=s.staff_id)
 GROUP BY store_id;
 
+#7g. Write a query to display for each store its store ID, city, and country.
 SELECT store_id, city, country FROM store s
 JOIN address a ON (s.address_id=a.address_id)
 JOIN city c ON (a.city_id=c.city_id)
 JOIN country cntry ON (c.country_id=cntry.country_id);
 
-SELECT c.name AS 'top_five', SUM(p.amount) AS 'Gross'
+#7h. List the top five genres in gross revenue in descending order. (Hint: you may need to use the following tables: category, film_category, inventory, payment, and rental.)
+SELECT c.name AS "Top Five", SUM(p.amount) AS "Gross"
 FROM category c
 JOIN film_category fc ON (c.category_id=fc.category_id)
 JOIN inventory i ON (fc.film_id=i.film_id)
@@ -118,4 +133,18 @@ JOIN rental r ON (i.inventory_id=r.inventory_id)
 JOIN payment p ON (r.rental_id=p.rental_id)
 GROUP BY c.name ORDER BY Gross  LIMIT 5;
 
-SELECT * FROM 'top_five';
+#8a. In your new role as an executive, you would like to have an easy way of viewing the Top five genres by gross revenue. Use the solution from the problem above to create a view. If you haven't solved 7h, you can substitute another query to create a view.
+CREATE VIEW top_five_genres AS
+SELECT c.name AS "Top Five", SUM(p.amount) AS "Gross"
+FROM category c
+JOIN film_category fc ON (c.category_id=fc.category_id)
+JOIN inventory i ON (fc.film_id=i.film_id)
+JOIN rental r ON (i.inventory_id=r.inventory_id)
+JOIN payment p ON (r.rental_id=p.rental_id)
+GROUP BY c.name ORDER BY Gross  LIMIT 5;
+
+#8b. How would you display the view that you created in 8a?
+SELECT * FROM top_five_genres;
+
+#8c. You find that you no longer need the view top_five_genres. Write a query to delete it.
+DROP VIEW top_five_genres
